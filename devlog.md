782eda90dbecdb682d32fbc2775d2ddf19440667
Aakrist Godar <aakrist.godar.1@gmail.com>
2026-03-06 22:45:45 -0600

    more changes

--- devlog.md
+++ devlog.md
@@ -36,7 +36,7 @@ END REPEAT
 ```
 
 # March 5th 2026 7:07 pm
-I wanted to get started with thinking about how to pick a mode properly. To implement this feature I knew I needed to use lambda function since it handles in line operations, passes functions as arugments, and also returns functions. I first used the map method to apply a function to every element, but I found a better methods called ormap that checks if any element of a list can satisfy a condition. I would just have ormap check if argument equals -b or --batch and have the it passed to the program.
+I wanted to get started with thinking about how to pick a mode properly. I had problems using the prompt? method that was given, it kept on giving me errors. So I had to think of a way to implement this feature better. I knew I needed to use lambda function since it handles in line operations, passes functions as arugments, and also returns functions. I first used the map method to apply a function to every element, but I found a better methods called ormap that checks if any element of a list can satisfy a condition. I would just have ormap check if argument equals -b or --batch and have the it passed to the program.
 
 # March 5th 2026 10:23 pm
 I decided to leave the program after the mode selection part. I think I will be adding a function to check for leading whitespace and add the history lookup feature. For now implemented, history feature, value parsing, and error checking. I didn't want to write long error logic every time so I made it a short function. History is built using cons, which means history is reversed internally. The lists are 0-index. For parsing numbers easier, I want to make it into a different function so I can have easier time debugging the code. For my next changes, I want to build my first draft to evaluate prefix notation and make parsing numbers.
@@ -46,3 +46,6 @@ I managed to implement parsing number function and parsing history. The program 
 
 # March 6th 2026 05:40 pm
 I added the prefix notation function, but it is running into errors. I knew the structure would be: operator operand operand and the algorithm must be recursive. Since there is a pattern, the algorithm evaluates the two operands and applies the operator. The algorithm checks for every operator and applies it to the two operands. The parsing is also made sequential because the program needs to be able to continue parsing the rest of the expression.
+
+# March 6th 2026 08:23 pm
+I added my main method that will run the entire function. I was able to run it in interactive mode but not batch mode. I had to look up on how to run it in bash, realized that I needed to add racket to my path environments and then run it that way. Even when I did that, I ran into a problem where the bash terminal would just end. So to solve this problem, I found a method called current-input-port that allows the input to only be taken through the terminal.
---
+++ prefix_calc.rkt
@@ -0,0 +1,119 @@
+#lang racket
+
+;; Pick your mode
+
+(define interactive? ; define a variable named interactive. Method interactive will be true unless batch flags are found
+  (not (ormap (λ (a) (or (string=? a "-b") ; ormap checks if any element of list satisfies the condition and lambda create function taking argument a
+                         (string=? a "--batch")))
+              (vector->list (current-command-line-arguments))))) ; convert vector to list and get the arguments passed
+;; Error checking
+(define (error-result)
+  (error "Invalid Expression"))
+
+;; History
+
+(define (history-ref history n) ; get the nth value from history
+  (let ([rev (reverse history)]) ; reverse function to reverse IDS for insertion
+    (if (or (< n 1) (> n (length rev)))
+        (error-result)
+        (list-ref rev (- n 1))))) ; if no error then return corresponding value
+
+;; Parsing value function
+
+(define (parse-number chars)
+  (define (loop cs acc) ; helper function
+    (cond [(and (pair? cs) (char-numeric? (first cs))) ; if next character exists and is numeric
+           (loop (rest cs) (cons (first cs) acc))] ; add digits and continue parsing
+          [else
+           (if (empty? acc)
+               #f ; this returns false
+               (list (string->number
+                      (list->string (reverse acc))) ; return remaining unparsed characters
+                     cs))]))
+  (loop chars '())) ; start recursion with empty accumulator
+
+(define (parse-history chars history)
+  (let ([num (parse-number chars)])
+    (if num (list (history-ref history (first num))
+                 (second num))
+           (error-result))))
+
+;; Whitespace
+
+(define (skip-ws chars)
+  (cond [(empty? chars) chars]
+        [(char-whitespace? (first chars))
+         (skip-ws (rest chars))]
+        [else chars]))
+
+;; Expression evaluation
+
+(define (prefix-eval chars history)
+  (let ([cs (skip-ws chars)])
+    (cond
+      [(empty? cs) (error-result)]
+
+      [(char=? (first cs) #\+) ; check for addition
+      (let* ([r1 (prefix-eval (rest cs) history)] ; check first operand
+             [r2 (prefix-eval (second r1) history)]) ; check second operand
+         (list (+ (first r1) (first r2)) ; add the values together 
+               (second r2)))]
+
+      [(char=? (first cs) #\*) ; check for multiplication
+      (let* ([r1 (prefix-eval (rest cs) history)] ; check first and second operand
+             [r2 (prefix-eval (second r1) history)]) ; 
+         (list (* (first r1) (first r2)) ; add the values together 
+               (second r2)))]
+
+      [(char=? (first cs) #\-) ; check for subtraction
+      (let* ([r1 (prefix-eval (rest cs) history)] ; check first and second operand
+             [r2 (prefix-eval (second r1) history)]) ; 
+         (list (/ (first r1) (first r2)) ; add the values together 
+               (second r2)))]
+
+      [(char=? (first cs) #\/) ; check for divison
+      (let* ([r1 (prefix-eval (rest cs) history)] ; check for the operands
+             [r2 (prefix-eval (second r1) history)])
+         (list (- (first r1) (first r2)) ; add the values together 
+               (second r2)))]
+
+      [(char=? (first cs) #\$)      ; check if token begins with '$'
+       (parse-history (rest cs) history)] ; parse and retrieve history value
+
+
+      [(char-numeric? (first cs))   ; check if token begins with digit
+       (or (parse-number cs)        ; attempt to parse number
+           (error-result))]         ; error if parsing fails
+
+      [else (error-result)])))      ; any other character results in error
+
+;; Main method
+(define (main history)
+  (when interactive? (display "> ")) ; when in interactive mode display prompt
+  (let loop ()
+    (let ([line (read-line (current-input-port))]) ; current-input-port allows it to only be read from terminal
+      (cond
+        [(eof-object? line) (void)] ; quit program if end of file
+        [(string=? line "quit") (void)] ; quit program if user types quit
+        [(string=? line "") (loop)]
+        [else
+         (with-handlers ([exn:fail? (λ (_) ; catch exception
+                                      (displayln "Error: expression is invalid")
+                                      (loop))])
+           (let* ([chars (string->list line)] ; convert input string to character list
+                  [res (prefix-eval chars history)]
+                  [remaining (skip-ws (second res))]) ; remove whitespace
+             (if (not (empty? remaining))
+                 (begin
+                   (displayln "Error: Invalid Expression")
+                   (main history)) ; restart loop
+                 (let* ([val (first res)]
+                        [new-history (cons val history)] ; get value and add result to history
+                        [id (+ 1 (length history))]) ; get new history ID
+                 
+                   (display id)
+                   (display ": ")
+                   (displayln (real->double-flonum val)) ; print result as float
+                   (main new-history)))))]))) ; 
+
+      
